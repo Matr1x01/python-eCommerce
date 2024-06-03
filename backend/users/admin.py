d591f075c1d7183908f1df3137a6aa4344218868
Mozharul Haq <mozharul.haq@appnap.io>
2024-06-03 19:26:08 +0600

    wip

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,15 +1,52 @@
 from django.contrib import admin
+from users.models import CustomUser, Customer, Staff
+from django import forms
+from backend.enums.user import User as UserEnum
 
-from users.models import CustomUser, Customer
+
+class UserTypeFilter(admin.SimpleListFilter):
+    title = 'User Type'
+    parameter_name = 'user_type'
+
+    def lookups(self, request, model_admin):
+        return CustomUser.USER_TYPE_CHOICES
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user_type=self.value())
+        return queryset
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    list_filter = (UserTypeFilter,)
+
+
+class CustomerForm(forms.ModelForm):
+    password = forms.CharField()
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerForm, self).__init__(*args, **kwargs)
+        if self.instance and self.instance.user:
+            self.fields['password'].initial = self.instance.user.password
+
+    def save(self, commit=True):
+        instance = super(CustomerForm, self).save(commit=False)
+        return instance
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('user',)
+
+
+class StaffAdmin(admin.ModelAdmin):
+    readonly_fields = ('user',)
+
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Staff, StaffAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -6,7 +6,7 @@ from backend.enums.user import User as UserEnum
 
 
 class CustomUser(AbstractUser):
-    USER_TYPE_CHOICES = [(u.value, u) for u in UserEnum]
+    USER_TYPE_CHOICES = [(u.value, u.name) for u in UserEnum]
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=UserEnum.STAFF.value)
 
 
@@ -14,17 +14,17 @@ class Customer(models.Model):
     user = models.OneToOneField(
         CustomUser,
         on_delete=models.CASCADE,
-        null=True, blank=True,
+        null=False, blank=False,
         related_name='customer',
         related_query_name='customer',
-        choices=[
-            (u.id, u.username) for u in
-            CustomUser.objects.filter(user_type=UserEnum.CUSTOMER.value)
-        ]
+        # choices=[
+        #     (u.id, u.username) for u in
+        #     CustomUser.objects.filter(user_type=UserEnum.CUSTOMER.value)
+        # ]
     )
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=20, unique=True)
-    gender = models.CharField(max_length=1, null=True, blank=True, choices=[(g.value, g.value) for g in Gender])
+    gender = models.CharField(max_length=1, null=True, blank=True, choices=[(g.value, g.name) for g in Gender])
     date_of_birth = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -36,7 +36,7 @@ class Customer(models.Model):
         return self.name
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if not self.user:
+        if not hasattr(self, 'user') or not self.user:
             user = CustomUser.objects.create_user(
                 username=self.phone,
                 password='123456',
@@ -45,10 +45,46 @@ class Customer(models.Model):
             self.user = user
         else:
             self.user.first_name = self.name
-            
+
         return super().save(force_insert, force_update, using, update_fields)
 
     class Meta:
         db_table = 'customers'
         verbose_name = 'Customer'
         verbose_name_plural = 'Customers'
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        null=False, blank=False,
+        related_name='staff',
+        related_query_name='staff',
+        # choices=[
+        #     (u.id, u.username) for u in
+        #     CustomUser.objects.filter(user_type=UserEnum.STAFF.value)
+        # ]
+    )
+    name = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, unique=True, null=False)
+
+    def get_user(self):
+        return self.user
+
+    def __str__(self):
+        return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not hasattr(self, 'user') or not self.user:
+            user = CustomUser.objects.create_user(
+                username=self.email,
+                password='123456',
+                first_name=self.name,
+                is_staff=True,
+                user_type=UserEnum.STAFF.value)
+            self.user = user
+        else:
+            self.user.first_name = self.name
+
+        return super().save(force_insert, force_update, using, update_fields)
